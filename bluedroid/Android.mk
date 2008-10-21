6c2d13d0c3a4505fdac56ae8d537a68218e072f1
The Android Open Source Project <initial-contribution@android.com>
2008-10-21 07:00:00 -0700

    Initial Contribution

---
+++ bluedroid/Android.mk
@@ -0,0 +1,43 @@
+ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+
+#
+# libbluedroid
+#
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	bluetooth.c
+
+LOCAL_C_INCLUDES := \
+	$(call include-path-for, bluez-libs)
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils
+
+LOCAL_MODULE := libbluedroid
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+#
+# bttest
+#
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := bttest.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+
+LOCAL_SHARED_LIBRARIES := libbluedroid
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE := bttest
+
+include $(BUILD_EXECUTABLE)
+
+endif
