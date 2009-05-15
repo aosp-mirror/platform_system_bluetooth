154f39e82e04e93cf2093ec8bd10de98ea03e32b
Nick Pelly <npelly@google.com>
2009-05-15 11:00:22 -0700

    Move bttest to new location in system/bluetooth/tools

--- bluedroid/Android.mk
+++ bluedroid/Android.mk
@@ -19,39 +19,3 @@ LOCAL_SHARED_LIBRARIES := \
 LOCAL_MODULE := libbluedroid
 
 include $(BUILD_SHARED_LIBRARY)
-
-
-#
-# bttest
-#
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := bttest.c
-
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
-
-LOCAL_SHARED_LIBRARIES := libbluedroid
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE := bttest
-
-include $(BUILD_EXECUTABLE)
-
-#
-# socktest
-#
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := socktest.c
-
-LOCAL_C_INCLUDES := \
-	system/bluetooth/bluez-clean-headers
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE := socktest
-
-include $(BUILD_EXECUTABLE)
--- tools/Android.mk
+++ tools/Android.mk
@@ -1,8 +1,26 @@
+LOCAL_PATH:= $(call my-dir)
+
+#
+# bttest
+#
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := bttest.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../bluedroid/include/
+
+LOCAL_SHARED_LIBRARIES := libbluedroid
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE := bttest
+
+include $(BUILD_EXECUTABLE)
+
 #
 # pipetest
 #
-
-LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
