40d3651f0ad7f3e25950ff7124547438bef57be7
Jaikumar Ganesh <jaikumar@google.com>
2011-03-22 10:37:34 -0700

    Update header files for Bluez upgrade to 4.89

--- bluez-clean-headers/bluetooth/hci_lib.h
+++ bluez-clean-headers/bluetooth/hci_lib.h
@@ -7,17 +7,41 @@
  ***   structures, and macros generated from the original header, and thus,
  ***   contains no copyrightable information.
  ***
+ ***   To edit the content of this header, modify the corresponding
+ ***   source file (e.g. under external/kernel-headers/original/) then
+ ***   run bionic/libc/kernel/tools/update_all.py
+ ***
+ ***   Any manual change here will be lost the next time this script will
+ ***   be run. You've been warned!
+ ***
  ****************************************************************************
  ****************************************************************************/
 #ifndef __HCI_LIB_H
 #define __HCI_LIB_H
+#ifdef __cplusplus
+#endif
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+struct hci_request {
+ uint16_t ogf;
+ uint16_t ocf;
+ int event;
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ void *cparam;
+ int clen;
+ void *rparam;
+ int rlen;
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+};
+struct hci_version {
+ uint16_t manufacturer;
+ uint8_t hci_ver;
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+ uint16_t hci_rev;
+ uint8_t lmp_ver;
+ uint16_t lmp_subver;
+};
+/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#ifdef __cplusplus
+#endif
+#endif
 
-#ifdef __cplusplus
-#endif
-#ifdef __cplusplus
-#endif
-static inline int hci_test_bit(int nr, void *addr)
-{
-	return *((uint32_t *) addr + (nr >> 5)) & (1 << (nr & 31));
-}
-#endif
